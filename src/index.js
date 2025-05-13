2faaf504e95825c9d4db7f89748043636740d8b5
nguyenanhhuy <nguyenanhhuy@luvina.net>
2025-05-13 12:20:01 +0700

    change logic js and new css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-var a = 49
-var b = 53
-var c;
-var sum = a + b
-console.log(sum)
+var term1 = 49// chỉnh sửa line code
+var term2 = 53 // chỉnh sửa line code
+// xóa line code;
+var sum = term1 + term2 // thay đổi code
+console.log(sum) // giữ nguyên
+var sub = term1 – term2 // thêm mới
